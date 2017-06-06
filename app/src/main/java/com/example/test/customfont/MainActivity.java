776c9c425d174811d41d3db8d4ecf36cdf9a542c
Milind123456 <milindbasavaraja96@gmail.com>
2017-06-06 21:51:30 +0530

    Updated with TextWatcher

--- app/src/main/java/com/example/test/customfont/MainActivity.java
+++ app/src/main/java/com/example/test/customfont/MainActivity.java
@@ -5,10 +5,15 @@ import android.support.v4.text.TextDirectionHeuristicCompat;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
     TextView t;
+    EditText tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
         t = (TextView)findViewById(R.id.my_text);
         Typeface customFont = Typeface.createFromAsset(getAssets(),"fonts/zeroes three.ttf");
         t.setTypeface(customFont);
+        tv = (EditText)findViewById(R.id.editText);
+        tv.addTextChangedListener(this);
+    }
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        Toast.makeText(this,"Before String",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        Toast.makeText(this,"On Change",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        int num = Integer.parseInt(s.toString());
+        if(num>100){
+            s.replace(0,s.length(),"100");
+        }
     }
 }
